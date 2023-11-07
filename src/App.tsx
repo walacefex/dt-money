abbee62bffdd63421a436260de31d66b9f543862
walacefex <walacefex01@gmail.com>
2023-11-07 00:56:40 -0300

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-     <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </ThemeProvider>
   )
 }
