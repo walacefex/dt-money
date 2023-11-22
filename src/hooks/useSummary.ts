ea4eb4407dff9ed4bb8d991e2e57350a4383b941
walacefex <walacefex01@gmail.com>
2023-11-22 20:07:06 -0300

    refactor: Calling the Transaction Summary Only Once

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useContextSelector } from 'use-context-selector';
 import { TransactionsContext } from '../contexts/TransactionsContext';
 
@@ -6,7 +7,8 @@ export function useSummary() {
     return context.transactions;
   });
 
-  const summary = transactions.reduce(
+  const summary = useMemo(() => {
+    return transactions.reduce(
     (acc, transaction) => {
       if (transaction.type === 'income') {
         acc.income += transaction.price;
@@ -24,6 +26,7 @@ export function useSummary() {
       total: 0,
     },
   );
+  }, [transactions]);
 
   return summary;
 }
